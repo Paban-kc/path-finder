e2303da315fb5124191919a69618863f91be6f22
Pawan <paban.kc67@gmail.com>
2023-09-30 18:14:41 +0545

    error solved

--- auth_common/Views/auth/logoutView.py
+++ auth_common/Views/auth/logoutView.py
@@ -3,16 +3,21 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
+from rest_framework import serializers  # Import the serializer module
 
 class LogoutView(GenericAPIView):
     """
-    Calls Django logout mehtod to logout current session.
+    Calls Django logout method to logout the current session.
     Accepts/Returns nothing.
     """
 
     permission_classes = (IsAuthenticated,)
     throttle_scope = "auth"
+    swagger_fake_view = True
+
+    def get_serializer_class(self):
+        # Use the generic Serializer class
+        return serializers.Serializer
 
     def post(self, request):
         logout(request)
