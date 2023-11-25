6eed316e4169b6b4ab9b82e9021e97f22f7bab64
Pawan <paban.kc67@gmail.com>
2023-11-25 20:22:50 +0545

    updated

--- auth_common/serializers/auth/userProfileSerializer.py
+++ auth_common/serializers/auth/userProfileSerializer.py
@@ -14,6 +14,7 @@ class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
         fields = "__all__"
+        ref_name = "OrganizationSerializer"
 
 
 class UserProfileSerializer(serializers.Serializer):
