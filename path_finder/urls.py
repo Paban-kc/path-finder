08ec9aab1533a33ff066f296b9fa3ce0ae3a6555
Pawan khatri <paban.kc67@gmail.com>
2024-06-28 09:41:51 +0545

    api maintain

--- path_finder/urls.py
+++ path_finder/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-documentation/", schema_view.with_ui("swagger", cache_timeout=0),
+    path("docs", schema_view.with_ui("swagger", cache_timeout=0),
          name="schema-swagger-ui"),
     path("users/", include("auth_common.urls")),
     # path("api/", include(("auth_common.urls"), namespace="api")),
