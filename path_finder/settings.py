4ad4a44e3652dcd695b78d30a3e39a445fd6ebcc
Paban kc <paban.kc67@gmail.com>
2025-04-15 23:30:50 +0545

    updated

--- path_finder/settings.py
+++ path_finder/settings.py
@@ -1,35 +1,30 @@
-# ruff: noqa: ERA001, E501
-"""Base settings to build other settings files upon."""
-
+import os
 from datetime import timedelta
 from pathlib import Path
+
+# from corsheaders.defaults import default_headers
+
 import environ
-import os
-BASE_DIR = Path(__file__).resolve(strict=True).parent
-APPS_DIR = BASE_DIR / "src"
+
 env = environ.Env()
-environ.Env.read_env(str(BASE_DIR / ".env"))
-
-
-CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS")
-CORS_ALLOWED_ORGINS = env.list("CORS_ALLOWED_ORIGINS")
-
-# from rest_framework_swagger.views import get_swagger_view
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tldu@8wwnl&!7hv891@(wh(bemset!_(f9w3x9w^5z(!@camj5"
+SECRET_KEY = env("SECRET_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") 
+DEBUG = env.bool("DEBUG")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS")
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
 
 # Application definition
 
