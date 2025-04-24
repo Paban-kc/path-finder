79128919194d9ac1e82e840dc8b72d931d95123f
Paban kc <paban.kc67@gmail.com>
2025-04-24 16:44:43 +0545

    changed settings

--- path_finder/settings.py
+++ path_finder/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
+    #"django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
